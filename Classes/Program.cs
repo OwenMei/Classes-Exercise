287e4f02c4efb02ea1059854fc97bed946565570
owen mei <owenzackmei@gmail.com>
2023-06-28 10:25:49 -0400

    init commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Car carOne = new Car() { make = "honda", model = "crv", year = 2016};
+            //asdf
+            Console.WriteLine($"Make: {carOne.make} \nModel: {carOne.model} \nYear: {carOne.year}");
         }
     }
 }
